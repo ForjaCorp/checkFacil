b941d0aaffada6e36e129c1abd68329f053db971
daame <barradeespacoe@gmail.com>
2025-06-14 11:04:34 -0300

    ajuste de segurança nas rotas de criação de usuarios. Adicionado a função que permite apenas que usuarios do tipo ADM_espaco criem outros usuarios. Precisa do Token para criar usuario

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as authController from '../controllers/authController.js';
-import { validarLogin, validarRegistro } from '../middleware/validarReqAuth.js';
+import { validarLogin, validarRegistro, permitirApenas, verificarTokenJWT } from '../middleware/validarReqAuth.js';
+import models from '../models/index.js';
 
 const router = express.Router();
 
-router.post('/register/convidado', validarRegistro, authController.registrarConvidado);
+router.post('/register/convidado', verificarTokenJWT ,permitirApenas(models.Usuario.TIPOS_USUARIO.ADM_ESPACO),validarRegistro, authController.registrarConvidado);
 
-router.post('/register/admEspaco', validarRegistro, authController.registrarAdmEspaco);
+router.post('/register/admEspaco', verificarTokenJWT, permitirApenas(models.Usuario.TIPOS_USUARIO.ADM_ESPACO) ,validarRegistro, authController.registrarAdmEspaco);
 
 router.post('/register/admFesta', validarRegistro, authController.registrarAdmFesta);
 router.post('/register/admEspaco', validarRegistro, authController.registrarAdmEspaco);
